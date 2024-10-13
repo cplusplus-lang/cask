a4088eb59b008a73bbf9ab19a7108b05712be8c2
Ismael <ijimenezm@hotmail.es>
2024-10-13 08:18:28 +0200

    Refactor CC

--- src/utils/library_index.hpp
+++ src/utils/library_index.hpp
@@ -1,13 +1,3 @@
-//              Copyright Cask Authors
-// Distributed under the Boost Software License, Version 1.0.
-//   (See accompanying file LICENSE.txt or copy at
-//        https://www.boost.org/LICENSE_1_0.txt)
-
-// SPDX-License-Identifier: BSL-1.0
-
-//
-// This file implements the library index.
-
 #pragma once
 
 #include <functional>
--- tests/src/commands/unit-build.cpp
+++ tests/src/commands/unit-build.cpp
@@ -1,10 +1,3 @@
-//              Copyright Cask Authors
-// Distributed under the Boost Software License, Version 1.0.
-//   (See accompanying file LICENSE.txt or copy at
-//        https://www.boost.org/LICENSE_1_0.txt)
-
-// SPDX-License-Identifier: BSL-1.0
-
 #include <array>
 #include <catch2/catch_test_macros.hpp>
 #include <commands/build.hpp>
